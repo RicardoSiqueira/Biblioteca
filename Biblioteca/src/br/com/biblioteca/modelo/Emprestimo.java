6baf6ea5083812b9bf290c5d5c954fa5b48ac355
26146595 <26146595@UNA009-L103-018.serlab.intra>
2018-11-13 22:02:27 -0300

    commit classes

---
+++ Biblioteca/src/br/com/biblioteca/modelo/Emprestimo.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.modelo;
+
+public class Emprestimo {
+	private String livros;
+	private String funcionarios;
+	private String data_Emprestimo;
+	private String devolucao;
+	private String calc_Devolucao;
+	private String limitar_livros;
+	private String emprestar_livros;
+	private String devolver_livros;
+
+}
---
+++ Biblioteca/src/br/com/biblioteca/modelo/Funcionario.java
@@ -0,0 +1,10 @@
+package br.com.biblioteca.modelo;
+
+public class Funcionario {
+	 private int matricula;
+	 private String nome;
+	 private int numero_OAB;
+	 private String tipo;
+		
+
+}
